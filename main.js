7fcf89e4bbb67ed6d051c6528a045e74c35e588a
Debfront <deborah_monteiro12@hotmail.com>
2024-02-23 12:28:46 -0300

    mudanças no layout css

---
+++ main.js
@@ -0,0 +1,23 @@
+// pegar as referencias com document
+
+const input_text = 
+const input_mensagem = 
+
+function btnCriptografar(){
+
+
+}
+
+function criptografar(string){
+    const resultado = string
+    .replaceAll ["e", "enter"]
+    
+
+    return resultado;
+}
+
+//function descriptor inverte a ordem das strings
+
+
+let text = 'texto de teste'
+console.log(criptografar(text))
