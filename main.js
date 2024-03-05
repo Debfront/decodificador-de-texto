0119301877bc622ebd852b992d3656aae8b10fef
Debfront <deborah_monteiro12@hotmail.com>
2024-03-05 20:17:50 -0300

    adicionando funçao criptografar e descriptografar

--- main.js
+++ main.js
@@ -1,23 +1,63 @@
 // pegar as referencias com document
 
-const input_text = 
-const input_mensagem = 
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
 
-function btnCriptografar(){
+// As "chaves" de criptografia que utilizaremos são:
+// A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "a" é convertida para "ai"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
 
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value= "";
 
 }
 
-function criptografar(string){
-    const resultado = string
-    .replaceAll ["e", "enter"]
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"],["u", "ufat"] ];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i= 0 ; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return stringEncriptada;
+}
+
+
+
+
+
+
+function btnDesencriptar(){
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensagem.value = textoDesencriptado;
+   textArea.value="";
+
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["enter", "e"], ["imes", "i"], ["ai", "a"], ["ober", "o"],["ufat", "u"] ];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i= 0 ; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][0])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1] );
+        }
+    }
+
+    return stringDesencriptada;
+}
+
+function copiar(){
+    var textoCop = document.getElementById('#input-texto');
     
-
-    return resultado;
+    document.execCommand('copy');
+    alert('Texto Copiado');
 }
-
-//function descriptor inverte a ordem das strings
-
-
-let text = 'texto de teste'
-console.log(criptografar(text))
